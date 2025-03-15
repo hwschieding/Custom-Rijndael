d8918a215ad215a6b26df9107f9f50b53d73495c
hwschieding <hwschieding@gmail.com>
2025-03-15 19:31:07 -0400

    Affine transformation calculation

--- rijndael.py
+++ rijndael.py
@@ -1,3 +1,6 @@
+def l_rot8(a, bits): # Circular shift left for bytes
+    return ((a << bits)|(a >> (8 - bits))) & 0xFF
+
 def galois_mul(a:int, b:int) -> int:
     a &= 255
     b &= 255
@@ -9,19 +12,28 @@ def galois_mul(a:int, b:int) -> int:
             product ^= a # Polynomial addition
         b >>= 1 # Discard 0th term
         carry = a & 128
-        a = (a << 1) & 255 # Multiply polynomial by x, bitwise AND by 11111111 masking to 8 bits
+        a = (a << 1) & 255 # Multiply polynomial by x, bitwise AND by 0b11111111 masking to 8 bits
         if carry:
             a ^= 0x1b # Irreducible polynomial constant
     return product
 
-def find_g_inverse(a):
+def find_galois_inverse(a: int) -> int: # Brute force method, checks every element in GF(2^8)
     for i in range(1, 256):
         if galois_mul(a, i) == 1:
             return i
 
+def affine_transform(a: int) -> int:
+    return a ^ l_rot8(a, 1) ^ l_rot8(a, 2) ^ l_rot8(a, 3) ^ l_rot8(a, 4) ^ 0x63
+
 if __name__ == "__main__":
 
-    print(galois_mul(0x53, 0xCA))
+    #print(galois_mul(0x53, 0xCA))
+
+    inv = find_galois_inverse(0x7a)
+    print(bin(inv))
+    affine = affine_transform(inv)
+    print(hex(affine))
+    print(bin(affine))
     
     '''
     checked = set()
--- test.py
+++ test.py
@@ -1,2 +1,6 @@
+import galois
 
-print(1 << 8)
+GF = galois.GF(2**8)
+
+print(GF(0x53) * GF(0xCA))
+print(0x53 * 0xCA)
