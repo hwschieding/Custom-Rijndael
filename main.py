e0c70fb25f564b162fbfb3dfb810c138bb60dc55
hwschieding <hwschieding@gmail.com>
2025-03-20 14:59:34 -0400

    inverse s-box

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from sbox import SBox
 
 if __name__ == "__main__":
-
+    # Temp code for testing S-Box
     s = SBox()
+    i_s = SBox(inverse=True)
+    for i in range(0, 256, 16):
+        print([f'{hex(n):4}' for n in s[i:i+16]])
     print()
     for i in range(0, 256, 16):
-        print([hex(n) for n in s[i:i+16]])
-
+        print([f'{hex(n):4}' for n in i_s[i:i+16]])
--- sbox.py
+++ sbox.py
@@ -1,6 +1,5 @@
 # Code for generating the Rijndael substitution box
 
-# TODO: Inverse S-Box generation
 # TODO: Add option to use precomputed S-Box from file instead of generating one
 
 from galois_math import find_galois_inverse
@@ -8,13 +7,17 @@ from galois_math import find_galois_inverse
 def l_rot8(a, bits) -> int:  # Circular shift left for bytes
     return ((a << bits) | (a >> (8 - bits))) & 0xFF
 
+# Following two functions are derived from algorithms found at https://en.wikipedia.org/wiki/Rijndael_S-box
 def affine_transform(a: int) -> int:
     return a ^ l_rot8(a, 1) ^ l_rot8(a, 2) ^ l_rot8(a, 3) ^ l_rot8(a, 4) ^ 0x63
 
-def compute_table() -> list:
+def inverse_affine_transform(s: int) -> int:
+    return l_rot8(s, 1) ^ l_rot8(s, 3) ^ l_rot8(s, 6) ^ 0x5
+
+def compute_forward_table() -> list:
     checked = set()
     table = [0] * 256
-    table[0] = 0x63
+    table[0] = 0x63 # Special case for 0 because it has no inverse
     for i in range(1, 256):
         if i not in checked:
             inverse = find_galois_inverse(i) # Because inverse(inverse) = i, we have found the inverse of two elements
@@ -23,8 +26,19 @@ def compute_table() -> list:
             checked.add(inverse)
     return table
 
+def compute_inverse_table() -> list:
+    table = [0] * 256
+    table[0x63] = 0 # Special case for 0, reversed
+    for i in range(0, 256):
+        if i != 0x63:
+            inv_affine = inverse_affine_transform(i)
+            table[i] = find_galois_inverse(inv_affine)
+    return table
+
+# Objectively awful use of classes, should probably think of a better way to utilize them
 class SBox:
-    def __init__(self):
-        self.table = compute_table()
+    def __init__(self, inverse: bool=False):
+        self.table = compute_inverse_table() if inverse else compute_forward_table()
+
     def __getitem__(self, index):
         return self.table[index]
