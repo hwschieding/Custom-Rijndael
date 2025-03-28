4a479fca42ed4f028555253a6c947f5a3e48c701
hwschieding <hwschieding@gmail.com>
2025-03-28 00:02:46 -0400

    Comments

--- galois_math.py
+++ galois_math.py
@@ -1,16 +1,16 @@
 # Handles mathematical operations in the Galois Finite Field ( GF(2^8) )
 
+# Multiplication
+# Algorithm found at https://en.wikipedia.org/wiki/Finite_field_arithmetic#Rijndael's_(AES)_finite_field
 def galois_mul(a:int, b:int) -> int:
     a &= 0xFF
     b &= 0xFF
     product = 0
     while a and b:
-        # print(f'{a=}, {bin(a)}')
-        # print(f'{b=}, {bin(b)}')
         if b & 1:
             product ^= a # Polynomial addition
         b >>= 1 # Discard 0th term
-        carry = a & 128
+        carry = a & 0x80
         a = (a << 1) & 0xFF # Multiply polynomial by x, bitwise AND by 0b11111111 masking to 8 bits
         if carry:
             a ^= 0x1b # Irreducible polynomial constant
@@ -21,4 +21,4 @@ def find_galois_inverse(a: int) -> int: # Brute force method, checks every eleme
         if galois_mul(a, i) == 1:
             return i
     # If there is no return, a is either 0 or outside GF(2^8).
-    # TODO: Handle exception above
+    # FIXME: Handle exception above
