e42bf147e034c32d9463d8319619191f31688e57
hwschieding <hwschieding@gmail.com>
2025-04-03 21:19:59 -0400

    Round key generation now uses int32s instead of bytearrays

--- src/key_schedule.py
+++ src/key_schedule.py
@@ -8,35 +8,42 @@ _ROUND_COUNT = {
     32: 15
 }
 
-def _l_word_rot(word: bytearray) -> bytearray:
-    out = word[1:]
-    out.append(word[0])
+def _l_rot32(x: int) -> int:
+    return ((x << 8) | (x >> 24)) & 0xFFFFFFFF
+
+def _array_to_num32(x: bytearray) -> int:
+    out = 0
+    for n in x:
+        out += n
+        out <<= 8
+    return out >> 8
+
+def _num32_to_array(x: int) -> bytearray:
+    out = bytearray()
+    mask = 0xFF000000
+    for i in range(3, -1, -1):
+        out.append((x & mask) >> (i * 8))
+        mask >>= 8
     return out
 
-def _get_words(key_bytes: bytearray) -> list[bytearray]:
+def _get_words(key_bytes: bytearray) -> list[int]:
     words = []
     for idx in range(0, len(key_bytes), 4):
-        words.append(key_bytes[idx : idx + 4])
+        words.append(_array_to_num32(key_bytes[idx : idx + 4]))
     return words
-
-def _word_xor(a: bytearray, b: bytearray) -> bytearray:
-    out = bytearray(4)
-    for i in range(4):
-        out[i] = a[i] ^ b[i]
-    return out
 
 class KeySchedule:
     _RCON = {
-        1: bytearray(b'\x01\x00\x00\x00'),
-        2: bytearray(b'\x02\x00\x00\x00'),
-        3: bytearray(b'\x04\x00\x00\x00'),
-        4: bytearray(b'\x08\x00\x00\x00'),
-        5: bytearray(b'\x10\x00\x00\x00'),
-        6: bytearray(b'\x20\x00\x00\x00'),
-        7: bytearray(b'\x40\x00\x00\x00'),
-        8: bytearray(b'\x80\x00\x00\x00'),
-        9: bytearray(b'\x1b\x00\x00\x00'),
-        10: bytearray(b'\x36\x00\x00\x00')
+        1: 0x01000000,
+        2: 0x02000000,
+        3: 0x04000000,
+        4: 0x08000000,
+        5: 0x10000000,
+        6: 0x20000000,
+        7: 0x40000000,
+        8: 0x80000000,
+        9: 0x1b000000,
+        10: 0x36000000
     }
     _SBOX: list = None
 
@@ -55,16 +62,18 @@ class KeySchedule:
         self.key_words = _get_words(key_bytes)
         self.round_keys = self._generate_round_keys()
 
-    def _sub_word(self, word: bytearray):
-        out = bytearray()
-        for i in word:
-            out.append(self._SBOX[i])
+    def _sub_word(self, word: int) -> int:
+        out = 0
+        for i in range(4):
+            shift = i * 8
+            mask = 0xFF << shift
+            out += self._SBOX[(word & mask) >> shift] << shift
         return out
 
     def _generate_round_keys(self) -> list[ByteMatrix16]:
         key_word_count = len(self.key_words)
         sub2_check = 4 % key_word_count
-        expanded_words: list[bytearray] = []
+        expanded_words: list[int] = []
         round_keys: list[ByteMatrix16] = []
         for round_num in range(0, self.rounds * 4, 4):
             round_words: bytearray = bytearray()
@@ -72,23 +81,26 @@ class KeySchedule:
                 if i < key_word_count:
                     word = self.key_words[i]
                 elif i % key_word_count == 0:
-                    g_word = self._sub_word(_l_word_rot(expanded_words[i - 1]))
-                    word = _word_xor(_word_xor(expanded_words[i - key_word_count], g_word), self._RCON[i // key_word_count])
+                    g_word = self._sub_word(_l_rot32(expanded_words[i - 1]))
+                    word = expanded_words[i - key_word_count] ^ g_word ^ self._RCON[i // key_word_count]
                 elif key_word_count > 6 and i % key_word_count == sub2_check:
-                    word = _word_xor(expanded_words[i - key_word_count], self._sub_word(expanded_words[i - 1]))
+                    word = expanded_words[i - key_word_count] ^ self._sub_word(expanded_words[i - 1])
                 else:
-                    word = _word_xor(expanded_words[i - key_word_count], expanded_words[i - 1])
-                print(f'iter {i}: {word=}')
+                    word = expanded_words[i - key_word_count] ^ expanded_words[i - 1]
+                print(f'iter {i}: {word=}, {hex(word)=}')
                 expanded_words.append(word)
-                round_words += word
+                round_words += _num32_to_array(word)
 
             round_keys.append(ByteMatrix16(round_words))
 
         return round_keys
 
+
 if __name__ == "__main__":
     s = compute_forward_sbox()
-    k = KeySchedule(bytearray(16), sbox=s)
-    print(k.key_size)
-    print(k.key_words)
-    print(k.round_keys)
+    b = bytearray('sixteen chars :)', 'utf-8')
+    k = KeySchedule(bytearray(32), sbox=s)
+    for i in range(0, len(b), 4):
+        for n in b[i : i + 4]:
+            print(f'{n:02x}', end='')
+        print(' ', end='')
