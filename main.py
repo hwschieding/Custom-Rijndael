c3150ecae1713e3e127151508f1736d5d1d836e7
hwschieding <hwschieding@gmail.com>
2025-04-07 20:41:26 -0400

    fixed main showcase

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ if __name__ == "__main__":
     print(f'{'*' * 67}\nAES Implementation - Week 2 | Showcase of new KeySchedule class\n{'*' * 67}\n')
     key_accepted = False
     while not key_accepted:
-        user_key = input('Enter 16 / 24 / 32-*BYTE* string (leave blank for default): ')
+        user_key = input('Enter 16 / 24 / 32-*BYTE* string (leave blank for 16-byte default): ')
         user_key = 'sixteen chars :)' if len(user_key) == 0 else user_key
         try:
             k = KeySchedule(user_key)
-            print('k = KeySchedule(user_key)')
+            print(f'\nuser_key = "{user_key}"')
+            print('k = KeySchedule(user_key)\n')
             key_accepted = True
         except ValueError as err:
             print(f'{err} -- Try again')
-    print(f"{k.print_keys()}")
+    k.print_keys()
+    input('End')
