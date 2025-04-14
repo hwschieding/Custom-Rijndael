088ad9b32f17160ff36ad1382664cd91b5e80e27
hwschieding <hwschieding@gmail.com>
2025-04-14 15:59:31 -0400

    Week 3 demonstration

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 # CSI 106 S25
-# Hunter Schieding 3/31/25
+# Hunter Schieding 4/14/25
 # Independent Study Spring 2025
 
-from src.key_schedule import KeySchedule
+from src.state import State
 
 if __name__ == "__main__":
-    print(f'{'*' * 67}\nAES Implementation - Week 2 | Showcase of new KeySchedule class\n{'*' * 67}\n')
-    key_accepted = False
-    while not key_accepted:
-        user_key = input('Enter 16 / 24 / 32-*BYTE* string (leave blank for 16-byte default): ')
-        user_key = 'sixteen chars :)' if len(user_key) == 0 else user_key
+    border_line = '*' * 67
+    print(f'{border_line}\nAES Implementation - Week 3 | Showcase of new state class & ShiftRows and MixColumns funcs\n{border_line}\n')
+    if input("Type 'y' to show unicode characters in matrix representations, leave blank for hex bytes: ").lower() == 'y':
+        State._debug_show_chars = True
+    print('\nState class - derived from ByteMatrix16 to handle more specific functions of the AES state')
+    user_state = None
+    while user_state is None:
         try:
-            k = KeySchedule(user_key)
-            print(f'\nuser_key = "{user_key}"')
-            print('k = KeySchedule(user_key)\n')
-            key_accepted = True
-        except ValueError as err:
-            print(f'{err} -- Try again')
-    k.print_keys()
+            user_str = input('Enter 16-BYTE or less string to be used for state: (leave blank for default): ')
+            user_str = 'sixteen chars :)' if user_str == "" else user_str
+            user_state = State(bytearray(user_str, 'utf-8'))
+        except ValueError as e:
+            print(f'{e}. Try again')
+    print(f'\nuser_state == {user_state}\n')
+    print(f'ShiftRows & MixColumns - Functions that act as the main source of diffusion in AES')
+    print(f'Starting state:\n{user_state.str_long()}')
+    user_state.shift_rows()
+    print(f'After user_state.shift_rows():\n{user_state.str_long()}')
+    user_state.mix_columns()
+    print(f'After user_state.mix_columns():\n{user_state.str_long()}')
+    input('Press Enter to continue...')
+    print('Reverse functions for ShiftRows & MixColumns exist for decryption')
+    user_state.mix_columns_inv()
+    print(f'After user_state.mix_columns_inv():\n{user_state.str_long()}')
+    user_state.shift_rows_inv()
+    print(f'After user_state.shift_rows_inv():\n{user_state.str_long()}')
     input('End')
